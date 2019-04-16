eecdd81d4fa9134536e4c3b34d78c9c71b351043
siyong.zhu <siyong.zhu@irdeto.com>
2019-04-16 01:50:46 -0400

    Add delete, hide and edit function

--- note_manager/execrise.js
+++ note_manager/execrise.js
@@ -1,5 +1,9 @@
 
 var btnAddNote = document.getElementById('add-btn')
+
+var ul = document.querySelector('ul')
+
+/**********************  Add item *******************************/
 btnAddNote.addEventListener('click', function(e){
     e.preventDefault();
     console.log('add a note')
@@ -34,4 +38,70 @@ btnAddNote.addEventListener('click', function(e){
 
         document.getElementById('add-input').value = ''
     } 
+})
+/** Edit and delete item */
+//Using event object e, you can get the DOM object mouse click upon
+//sidenote: 
+ul.addEventListener('click', function(e){
+    //console.log(this)  <-- sidenote: `this` only returns ul no matter which DOM you click on
+    //Now we can check the class of the elem DOM object clicked upon
+    if (e.target.classList[1] === 'fa-pencil-square-o'){
+        var parentPar = e.target.parentNode
+        parentPar.style.display='None' //hide the current element 
+        var text = parentPar.previousElementSibling //get <p> elem 
+        var input = parentPar.nextElementSibling //get <input> elem
+        input.style.display='block'
+        input.value=text.textContent
+
+        input.addEventListener('keypress', function(e){
+            //event object for keypress has key and keyCode attribute
+            //that keeps track of each keyboard button pressed
+            if (e.keyCode === 13) { // 13 is ENTER key
+                if(input.value === ''){
+                    //propangate to parent node
+                    var li = input.parentNode
+                    var ul = li.parentNode
+                    //from parent node, remove the element
+                    ul.removeChild(li)
+                } else {
+                    text.textContent = input.value
+                    parentPar.style.display = 'block'
+                    input.style.display = 'none'
+                }              
+            }
+        })
+    }else if (e.target.classList[1] === 'fa-times'){
+        var li = e.target.parentNode.parentNode
+        var ul = li.parentNode
+        ul.removeChild(li)
+    }
+})
+
+var hideItem = document.getElementById('hide')
+hideItem.addEventListener('click', function(e){
+    var label = document.querySelector('#hide-list label')
+    if(hideItem.checked){
+        label.textContent = 'Unhide'
+        ul.style.display = 'none'
+    }else{
+        label.textContent = 'Hide'
+        ul.style.display = 'block'
+    }
+})
+
+var searchInput = document.querySelector('#search-note input')
+
+searchInput.addEventListener('keyup', function(e){ //key is oppsite of keypress
+    console.log(e.keyCode+' is released')
+    var searchChar = e.target.value.toUpperCase()
+    var notes = ul.getElementsByTagName('li')
+
+    Array.from(notes).forEach(function(note){
+        var parText = note.firstElementChild.textContent
+        if(parText.toUpperCase().indexOf(searchChar) != -1){
+            note.style.display = 'block'
+        }else{
+            note.style.display = 'none'
+        }
+    })
 })
